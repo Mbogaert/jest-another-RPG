3a96c8fdd9b655e83e1107d9b58dd572554bb6fb
Mathew B <bogaert@email.arizona.edu>
2020-11-28 16:05:16 -0700

    finished potion TDD

--- __tests__/random.test.js
+++
@@ -1,6 +0,0 @@
-const { TestScheduler } = require('jest');
-const checkIfEqual = require('../lib/random.js');
-
-test('check if 10 is equal to 10', () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
-});
