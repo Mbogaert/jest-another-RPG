e9aef0653f039829fe8f8cf62bd18f3a819b2c75
Mathew B <bogaert@email.arizona.edu>
2020-11-28 16:43:50 -0700

    player stats and inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,7 +5,7 @@ jest.mock('../lib/Potion');
 console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 
 test('create a player object', () => {
     const player = new Player('Mathew');
@@ -17,4 +17,23 @@ test('create a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("get player's stats as objects", () => {
+    const player = new Player('Mathew');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('Mathew');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
